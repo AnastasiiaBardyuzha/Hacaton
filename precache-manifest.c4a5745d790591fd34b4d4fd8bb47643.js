366eebdfab61b74b4a23e952e0075af9650dfda7
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-02-19 17:56:06 +0200

    Updates

---
+++ precache-manifest.c4a5745d790591fd34b4d4fd8bb47643.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5c388de304e4eedd8c5124f2dc9e970",
+    "url": "/hakaton/index.html"
+  },
+  {
+    "revision": "d4e4e6cf4102a2d8f3ba",
+    "url": "/hakaton/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "accb14dffd145181b06a",
+    "url": "/hakaton/static/js/2.d5bcec2c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hakaton/static/js/2.d5bcec2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4e4e6cf4102a2d8f3ba",
+    "url": "/hakaton/static/js/main.f181e569.chunk.js"
+  },
+  {
+    "revision": "7e2458c46dfcaf784a3f",
+    "url": "/hakaton/static/js/runtime-main.c20b5d4c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hakaton/static/media/logo.5d5d9eef.svg"
+  }
+]);
